e417cd53a6738ec0ff97c99c9340dc8b62940d96
lawless1213 <andruha2103@gmail.com>
2025-01-14 21:06:27 +0200

    list

--- src/components/ExerciseCard/index.jsx
+++ src/components/ExerciseCard/index.jsx
@@ -1,30 +1,65 @@
-import { Group, Card, Title, Text, Image, Badge } from '@mantine/core';
-import s from './index.module.scss';
+import { Group, Card, Title, Badge, Loader } from '@mantine/core';
 
-const ExerciseCard = ({name, equipment, bodyPart, preview}) => {
+import s from './index.module.scss';
+import { useState } from 'react';
+
+const ExerciseCard = ({name, equipment, bodyPart, preview, video}) => {
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [isLoadingImg, setIsLoadingImg] = useState(true);
+
+	const handleImageLoad = () => {
+    setIsLoadingImg(false);
+  };
+
+	const handleMouseOver = (event) => {
+    const video = event.currentTarget.querySelector("video");
+		if (video && video.paused && !isPlaying) {
+			video.playbackRate = 2; 
+			video.play();
+			setIsPlaying(true);
+		}
+  };
+
+  const handleMouseOut = (event) => {
+    const video = event.currentTarget.querySelector("video");
+    if (video && isPlaying) {
+			video.pause();
+			setIsPlaying(false);
+			video.currentTime = 0;
+		}
+  };
+
 
 	return (
-		<Card
-			shadow="sm"
-      padding="sm"
-		>
-			<Group gap="sm">
-				<Badge>{equipment}</Badge>
-				<Badge variant="light">{bodyPart}</Badge>
-			</Group>
-			<Card.Section >
-				<Image
+		<>
+			<Card
+				shadow="sm"
+				padding="md"
+				className={s.Card}
+				onMouseOver={handleMouseOver} 
+				onMouseOut={handleMouseOut}
+			>
+				<Group gap="sm">
+					<Badge>{equipment}</Badge>
+					<Badge variant="light">{bodyPart}</Badge>
+				</Group>
+				<Card.Section
+					className={s.Preview} 
 					mt="md"
 					mb="md"
-					height="160px"
-					fit="contain" 
-					src={preview} alt={name} 
-				/>
-      </Card.Section>
-			<Title ta="center" order={3}>
-				{name}
-			</Title>
-		</Card>
+				>
+					{isLoadingImg && (
+						<Loader/>
+					)}
+					<video width="100%" src={video} muted loop onLoad={handleImageLoad}>
+						Your browser does not support the video tag.
+					</video>
+				</Card.Section>
+				<Title ta="center" order={3}>
+					{name}
+				</Title>
+			</Card>
+		</>
 	)
 } 
 
--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -16,6 +16,7 @@ const Exercises = observer(() => {
 			<ExerciseCard
 				name = {item.name}
 				preview = {item.preview}
+				video = {item.video}
 				equipment = {item.equipment}
 				bodyPart = {item.bodyPart}
 			/>
