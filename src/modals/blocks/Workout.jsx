233cb52907dab1bfae08557b09092e42dd12661b
lawless1213 <andruha2103@gmail.com>
2025-05-13 20:44:36 +0300

    add select exercises to workout

--- src/modals/blocks/Workout.jsx
+++ src/modals/blocks/Workout.jsx
@@ -2,11 +2,15 @@ import { useForm } from '@mantine/form';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import workoutService from '../../services/workoutService';
-import { Button, ColorInput, ColorPicker, Group, Stack, Textarea, TextInput } from '@mantine/core';
+import { Button, ColorInput, ColorPicker, Stepper, Group, Stack, Textarea, TextInput } from '@mantine/core';
 
 function Workout({ closeModal }) {  
 	const { t } = useTranslation();
 	const [loading, setLoading] = useState(false);
+
+	const [active, setActive] = useState(0);
+  const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
+  const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
 	const form = useForm({
 		initialValues: {
@@ -36,39 +40,57 @@ function Workout({ closeModal }) {
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
-				<Stack gap="md">
-					<Group>
-						<ColorInput 
-							w="130px" 
-							disallowInput 
-							placeholder='Color'
-							{...form.getInputProps('color')}
-						/>
-						<TextInput
-							flex="1"
-							placeholder="Workout name"
-							{...form.getInputProps('name')}
-						/>
-					</Group>
-					
-					<Textarea
-						placeholder="Workout description"
-						h="100%"
-						{...form.getInputProps('description')}
-					/>
-					
-					<Group justify="flex-end">
-						<Button fullWidth size='m' type="submit" radius="xl">
-							{
-								loading ?
-								"Loading.." :
-								"Create workout"
-							} 
-						</Button>
-					</Group>
-				</Stack>
-			</form>
+		<Stepper iconSize={32} active={active} onStepClick={setActive} allowNextStepsSelect={false}>
+        <Stepper.Step label="Create" description="Create a program">
+          <form onSubmit={handleSubmit}>
+						<Stack gap="md">
+							<Group>
+								<ColorInput 
+									w="130px" 
+									disallowInput 
+									placeholder='Color'
+									{...form.getInputProps('color')}
+								/>
+								<TextInput
+									flex="1"
+									placeholder="Workout name"
+									{...form.getInputProps('name')}
+								/>
+							</Group>
+							
+							<Textarea
+								placeholder="Workout description"
+								h="100%"
+								{...form.getInputProps('description')}
+							/>
+							
+							{/* <Group justify="flex-end">
+								<Button fullWidth size='m' type="submit" radius="xl">
+									{
+										loading ?
+										"Loading.." :
+										"Create workout"
+									} 
+								</Button>
+							</Group> */}
+						</Stack>
+					</form>
+        </Stepper.Step>
+        <Stepper.Step label="Select" description="Select exercises">
+          Step 2 content: Select exercises
+        </Stepper.Step>
+        <Stepper.Step label="Confirm" description="Confirm program">
+          Step 3 content: Confirm program
+        </Stepper.Step>
+        <Stepper.Completed>
+          Completed, click back button to get to previous step
+        </Stepper.Completed>
+      </Stepper>
+
+      <Group justify="center" mt="xl">
+        <Button variant="default" onClick={prevStep}>Back</Button>
+        <Button onClick={nextStep}>Next step</Button>
+      </Group>
 		</>
 	)
 }
