8bde4d6392209ddce5ae2e04f4700a6c29834647
lawless1213 <andruha2103@gmail.com>
2025-07-09 19:31:11 +0300

    add schedule calendar component

--- src/components/workoutSchedule/index.jsx
+++ src/components/workoutSchedule/index.jsx
@@ -1,42 +1,99 @@
-import { DatePicker } from '@mantine/dates';
 import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_USER_SCHEDULE } from '../../queries/schedule';
 import dayjs from "dayjs";
+import isoWeek from 'dayjs/plugin/isoWeek';
+import { ActionIcon, Button, Group, Text, Indicator  } from '@mantine/core';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 
+dayjs.extend(isoWeek);
 
 function WorkoutSchedule() {
-		// const [value, setValue] = useState(new Date());
+	const [value, setValue] = useState(new Date());
 
-  const [range, setRange] = useState(["2025-07-02", "2025-07-10"]); // [start, end]
+	const [weekOffset, setWeekOffset] = useState(0);
 
-  // Формуємо дати для запиту, якщо обрано обидві
-  const startDate = range[0] ? dayjs(range[0]).format('YYYY-MM-DD') : null;
-  const endDate = range[1] ? dayjs(range[1]).format('YYYY-MM-DD') : null;
+	const currentWeekStart = dayjs().add(weekOffset, 'week').startOf('isoWeek');
 
-	// const startDate = "2025-07-02";
-  // const endDate = "2025-07-05";
+	const days = Array.from({ length: 7 }, (_, i) =>
+		currentWeekStart.add(i, 'day')
+	);
+	const isSelected = (date) =>
+    value && dayjs(value).isSame(date, 'day');
+
+	const startDate = days[0].format('YYYY-MM-DD');
+	const endDate = days[6].format('YYYY-MM-DD');
 
   const { data, loading, error } = useQuery(GET_USER_SCHEDULE, {
-    skip: !startDate || !endDate, // не робити запит, якщо не обрано обидві дати
     variables: { startDate, endDate }
   });
 
   const schedule = data?.getUserSchedule || [];
-
 	console.log('schedule', schedule);
 	
-
   return (
-    <>
-      <DatePicker
-        type="range"
-        hideOutsideDates
-        size="md"
-        allowDeselect
-        value={range}
-      />
-    </>
+    <Group spacing="xs" align="center" position="center" wrap='nowrap' justify='center'>
+      <ActionIcon
+        variant="transparent"
+        onClick={() => setWeekOffset((w) => w - 1)}
+        size="lg"
+      >
+        <IconChevronLeft />
+      </ActionIcon>
+
+      <Group spacing="xs" align="center" position="center" justify='center'>
+				{days.map((day) => {
+					const dayStr = day.format('YYYY-MM-DD');
+					const scheduleDay = schedule.find(d => d.date === dayStr);
+
+					if(scheduleDay) {
+						return (
+							<Indicator processing  color={scheduleDay.workouts[0].workout.color}>
+								<Button
+									key={dayStr}
+									variant={isSelected(day) ? 'outline' : 'default'}
+									radius="s"
+									size="md"
+									styles={{
+										label: { flexDirection: 'column' },
+									}}
+								>
+									{/* <Text size="xs" ta="center">{day.format('dd')}</Text> */}
+									<Text size="sm" ta="center" fw={500}>{day.format('DD.MM')}</Text>
+									{/* Відображаємо інфу про тренування, якщо є */}
+									<Text size="xs" c="green">
+										{scheduleDay.workouts.length} трен.
+									</Text>
+								</Button>
+							</Indicator>
+						)
+					}
+
+					return (
+						<Button
+							key={dayStr}
+							variant={isSelected(day) ? 'outline' : 'default'}
+							radius="s"
+							size="md"
+							styles={{
+								label: { flexDirection: 'column' },
+							}}
+						>
+							<Text size="xs" ta="center">{day.format('dd')}</Text>
+							<Text size="sm" ta="center" fw={500}>{day.format('DD.MM')}</Text>
+						</Button>
+					);
+				})}
+			</Group>
+
+      <ActionIcon
+        variant="transparent"
+        onClick={() => setWeekOffset((w) => w + 1)}
+        size="lg"
+      >
+        <IconChevronRight />
+      </ActionIcon>
+    </Group>
   );
 }
 
